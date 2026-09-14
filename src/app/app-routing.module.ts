99bc8ee7dce04e40eceef77d9d9e400daa83b021
Gesislaine <lgesislaine@gmail.com>
2026-09-14 13:27:09 -0300

    Adicionando a  rota de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,23 +7,36 @@ const routes: Routes = [
     redirectTo: 'visitante',
     pathMatch: 'full'
   },
+
   {
     path: 'visitante',
-    loadChildren: () => import('./visitante/visitante.module').then(m => m.VisitantePageModule)
+    loadChildren: () =>
+      import('./visitante/visitante.module').then(m => m.VisitantePageModule)
   },
+
   {
     path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+    loadChildren: () =>
+      import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
     path: 'detalhes-plano/:id',
     loadChildren: () => import('./detalhes-plano/detalhes-plano.module').then( m => m.DetalhesPlanoPageModule)
+  },
+
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then(m => m.LoginPageModule)
   }
 
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
